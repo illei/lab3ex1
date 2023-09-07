78f3b287e87af00e4e5e741bebbd636f8703ad3c
Illei Adrian <illei.adrian@yahoo.com>
2023-09-07 16:34:40 +0200

    Add project files.

---
+++ lab3ex1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace lab3ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numar = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(sumaCifrelor(numar));
+        }
+
+       static int sumaCifrelor(int x)
+        {
+            int suma = 0;
+
+            while (x != 0)
+            {
+
+
+
+
+
+
+                suma = suma + x % 10;
+
+                x = x / 10;
+
+
+            }
+            
+            return suma;
+        }
+
+       
+    }
+}
